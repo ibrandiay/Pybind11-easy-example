a70dd34ff726c8dd8b2b1a6da15f366ea759ed14
ibrandiay <baye102@hotmail.fr>
2022-04-06 17:43:35 +0200

    gd

--- example.cpp
+++ example.cpp
@@ -3,8 +3,12 @@
 int multiply(int i, int j) {
     return i * j;
 }
+int addition(int i , int j ) { 
+   return i + j  ; 
+   }
 
 PYBIND11_MODULE(example, m) {
     m.doc() = "pybind11 example plugin"; // optional module docstring
     m.def("multiply", &multiply, "A function which multiplies two numbers");
+    m.def("addition", &addition , "A fonction which addition two numers" );
 }
